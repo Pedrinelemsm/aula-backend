d5cc4beac67e6aad6a152a47271fde3824b18756
unknown <2314290138@ACADEMICO.IESB.BR>
2024-03-15 10:51:22 -0300

    finalizando aula05

--- aula05/router.js
+++ aula05/router.js
@@ -5,5 +5,38 @@ router.get('/produtos', function(req, res){
     res.json([])
 });
 
+router.get("/produtos/:produtoId", function(req, res){
+    if (req.params.produtoId == 1){
+        res.json({})
+       }   else {
+        res.status(404).json({msg: "Produto nao encontrado"});
+    }
+    
+    res.json({});
+});
+router.post("/produtos", function(req, res){
+    if (req.body && req.body.nome && req.body.preco) {
+        res.status(201).json({});
+    } else {
+        res.status(422).json({msg: "Nome e/ou preco do produto necessarios"});
+    }
+})
+
+router.put("/produtos/:produtoId", function(req, res){
+    if (req.params.produtoId == 1) {
+        res.json({});
+    } else {
+        res.status(404).json({msg: "Produto nao encontrado"});
+    }
+})
+
+router.delete("/produtos/:produtoId", function(req, res){
+    if (req.params.produtoId == 1) {
+        res.status(204).end();
+    } else {
+        res.status(404).json({msg: "Produto nao encontrado"});
+    }
+})
+
 module.exports = router;
 
